dbb0e965cd222ab72781ee641e1bb5fef6adea83
Kirsty <kirsty02last@gmail.com>
2024-01-07 18:21:38 +0000

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1> Testing</h1>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Kanban</title>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700&display=swap" rel="stylesheet">    <body class="antialiased">
+        <div id="app">
+            <example-component></example-component>
+
+        </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
